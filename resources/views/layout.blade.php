56895279b2275b011027ac37f684a6397191a8ee
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-05-10 22:14:51 -0300

    primeiro commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+         
+        <title>Rimo</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
+      
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+       @livewireStyles   
+      
+       
+    </head>
+    <body>
+        @yield('content')
+
+        @livewireScripts
+    </body>
+</html>
