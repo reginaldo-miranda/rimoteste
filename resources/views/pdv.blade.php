36c583d75a24a6b042670b2fecf513a2a2f3d0e2
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-05-14 17:21:25 -0300

    firma 1405211721

--- resources/views/pdv.blade.php
+++ resources/views/pdv.blade.php
@@ -1,2 +1,11 @@
 @extends('layout')
 @include('menu')
+
+<div class="container">
+    <div id="telapdv">
+        <label><h3>Produtos</h3></label><br>
+        <label><h3>foto e logo</h3></label>
+        <div id="telaprod">
+             <textarea id="areavenda" row="100" cols="70" name="areavenda"></textarea>
+        </div>
+</div>
