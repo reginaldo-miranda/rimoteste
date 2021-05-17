f4a04bf8d7352925fd598c3c6b8028cee1f03994
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-05-17 17:03:14 -0300

    170521 1703

--- resources/views/pdv.blade.php
+++ resources/views/pdv.blade.php
@@ -1,8 +1,6 @@
 @extends('layout')
 @include('menu')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 <div class="container">
     <div id="telapdv">
         <label><h3>Produtos</h3></label><br>
@@ -10,18 +8,5 @@
         <div id="telaprod">
              <textarea id="areavenda" row="100" cols="70" name="areavenda"></textarea>
         </div>
-=======
-=======
->>>>>>> ef25bd21d04ffcc1413492c0c03a8b642e26c277
+
 <div>>
-
-<label>fotos</label>
-<textarea id="story" name="story" rows="15" cols="80">
-
-</textarea>
-
-<<<<<<< HEAD
->>>>>>> ef25bd21d04ffcc1413492c0c03a8b642e26c277
-=======
->>>>>>> ef25bd21d04ffcc1413492c0c03a8b642e26c277
-</div>
