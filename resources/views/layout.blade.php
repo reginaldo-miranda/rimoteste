b7b06f2d0c3e70f49e917549f30a5968bf5c4923
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-05-18 15:10:18 -0300

    firma 180521 1510

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
         <title>Rimo</title>
         <link href="{{ asset('css/app.css')}}" rel="stylesheet"> 
         <link href="{{ asset('css/telapdv.css')}}" rel="stylesheet">
+        <link href={{ asset('css/menu.css')}} rel="stylesheet">
       
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
