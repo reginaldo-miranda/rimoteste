8b09bc6ebe90fb60d9262a56a9eec39f06bfc59a
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-05-21 16:04:01 -0300

    firma 210521 1603

--- app/Http/Livewire/Pdvcomponent.php
+++ app/Http/Livewire/Pdvcomponent.php
@@ -3,14 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\models\produto;
+use App\Http\Livewire\produtos;
+use Illuminate\Pagination\Paginator;
+use Livewire\WithPagination;
+
+
+use Livewire\Component;
 
 class Pdvcomponent extends Component
 {
+    public $prod_id, $descricao, $grupo, $pvenda, $buscapdv;
+
     public function mount($id){
 
-     $busca = produto::find($id);
+     $buscapdv = produto::find($id);
 
-     return view('livewire.pdvcomponent')->layout('pdv');
+     return " aqui achei "; //view('livewire.pdvcomponent')->layout('pdv');*/
 
     }
 
@@ -18,6 +27,11 @@ class Pdvcomponent extends Component
     {
 
         return view('livewire.pdvcomponent');
+    }
+
+    public function buscar(){
+
+        return 'achei aqui';
     }
 }
 
