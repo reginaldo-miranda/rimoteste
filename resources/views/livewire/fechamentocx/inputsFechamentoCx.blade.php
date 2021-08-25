9e395be1cbaa80b400d8c41dd64b952d211ecdba
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-08-25 17:20:40 -0300

    firma 250821 17

--- resources/views/livewire/fechamentocx/inputsFechamentoCx.blade.php
+++ resources/views/livewire/fechamentocx/inputsFechamentoCx.blade.php
@@ -13,7 +13,7 @@
           
     <label>procurar produtos</label>
     <input type="text" class="form-control" wire:model="searchprod">  
-    @error("searchprod")<span>{{ $message }}</span> @enderror --}}
+    @error("searchprod")<span>{{ $message }}</span> @enderror 
  
     <div class="inputes">valor total:
         <input type="text"  class="input-group " id="inputvlrtotal" name="inputvlrtotal" 
@@ -30,21 +30,33 @@
     </div>
     <div class="inputes">Pix   :
         <input id="inputpix" type="text" class="input-group" >
+    </div> --}}
+    <form action="" method="post">
+      N1:  <input type="text"  id="n1" value="{{ $total }}" /> <br>
+      dinh:<input type="text"  id="n2" value="{{ $inputdinh }}"  onchange="calcular()"/> <br>
+      Pix: <input type="text"  id="n3" value="{{ $inputpix }}" onchange="calcular()" /> <br>
+  
+    </form>
+    
+    <div 
+        id="resultado">
     </div>
 
     
     @include('livewire.fechamentocx.botaoFechamentoCx')
+
    {{--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>  --}}
-    <script>
+ <script>
+
+  //var j = parseFloat(Math.round(29.6 * 100) / 100).toFixed(2);
+  /*
     $(document).ready(function() {
         $("#inputvlrtotal,#inputdinh").on('keyup', function() {
       
           var total  = parseFloat($('#inputvlrtotal').val()) || 0;
           var dinh   = parseFloat($('#inputdinh').val()) || 0;
-         
           var totalgeral = total-dinh;
           $('#inputvlrtotal').val(totalgeral);
-          var totalg = total-cartao;
       
         });
       });
@@ -57,11 +69,10 @@
           var cartao = parseFloat($('#inputcartao').val()) || 0;
           var totalgeral = total-cartao;
           $('#inputvlrtotal').val(totalgeral);
-         
-          
        
         });
       });
+
       $(document).ready(function() {
         $("#inputvlrtotal,#inputpix").on('keyup', function() {
       
@@ -69,12 +80,47 @@
           var pix = parseFloat($('#inputpix').val()) || 0;
           var totalgeral = total-pix;
           $('#inputvlrtotal').val(totalgeral);
-                  
-                      
       
         });
       });
+      
+     
+      function calcular(){
+       var n1 = parseInt(document.getElementById('inputvlrtotal')).value);
+       var n2 = parseInt(document.getElementById('inputdinh')).value);
+       document.getElementById('inputvlrtotal').innerHTML = n1 - n2;
 
-</script>
-    
-{{-- https://pt.stackoverflow.com/questions/211767/somar-inputs-com-jquery-e-tempo-real  calculo de caixa--}}
+} 
+*/
+function calcular() {
+  var n1 = parseInt(document.getElementById('n1').value, 10);
+  var n2 = parseInt(document.getElementById('n2').value, 10);
+  document.getElementById('resultado').innerHTML = n1-n2;
+
+ 
+}
+
+</script>   
+
+
+
+{{-- https://pt.stackoverflow.com/questions/211767/somar-inputs-com-jquery-e-tempo-real  calculo de caixa
+{{-- 
+function calcular() {
+  var n1 = parseInt(document.getElementById('n1').value, 10);
+  var n2 = parseInt(document.getElementById('n2').value, 10);
+  document.getElementById('resultado').innerHTML = n1 + n2;
+}
+<form action="" method="post">
+  N1: <input type="text" id="n1" value="10" /> <br>
+  N2: <input type="text" id="n2" value="5" onblur="calcular()" /> <br>
+</form>
+
+<div id="resultado"></div>
+
+
+
+
+
+--}}
+
