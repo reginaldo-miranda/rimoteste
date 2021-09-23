506aa6963ebf1b4beb4c189c6c15f9f4461628d1
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-09-23 17:26:59 -0300

    firma 2309 17

--- resources/views/livewire/fechamentocx/inputsFechamentoCx.blade.php
+++ resources/views/livewire/fechamentocx/inputsFechamentoCx.blade.php
@@ -1,24 +1,27 @@
 
-      <div class="inputes">valor total:
-         <input type="text"  id="n1" value="{{ $total }}" />
+      <div class="inputes">
+         <div>
+            <label>Valor total</label>
+         </div>
+         <input type="text" class="btnfechar" id="n1" value="{{ $total }}" />
       </div>  
 
       
      <div class="inputes">
-        <label id="troco">Troco</label>
-        <input type="text" id="resto"> 
+        <label id="troco">Troco:</label>
+        <input type="text" class="btnfechar" id="resto"> 
       </div>
 
       <div class="inputes">dinheiro:
-         <input type="text"  id="n2"  value="{{ number_format($inputdinh, 2, ',' ,  '.')}}" onchange="calcular()"/> 
+         <input type="text" class="btnfechar" id="n2"  value="{{ number_format($inputdinh, 2, ',' ,  '.')}}" onchange="calcular()"/> 
       </div>
 
-      <div class="inputes">Pix 
-          <input type="text"  id="n3" value="{{ number_format($inputpix,2, ',', '.') }}"    onchange="calcular()"/> 
+      <div class="inputes">Pix: 
+          <input type="text" class="btnfechar" id="n3" value="{{ number_format($inputpix,2, ',', '.') }}"    onchange="calcular()"/> 
       </div> 
       
-      <div class="inputes">Cartao   :
-         <input type="text"  id="n4" value="{{ number_format($inputcartao,2, ',', '.') }}" onchange="calcular()"/> <br>
+      <div class="inputes">Cartao:
+         <input type="text" class="btnfechar" id="n4" value="{{ number_format($inputcartao,2, ',', '.') }}" onchange="calcular()"/> <br>
       </div>
            
        <div id="resultado"></div>
